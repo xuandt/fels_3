b0dbae94bde5c5da2cd6ebbed1a6584ee5f0e78c
nguyentuyen <nguyentuyen@nguyentuyen-Satellite-L740.(none)>
2014-05-05 09:15:21 +0700

    framwork-signup-signin-signout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,9 @@ module SessionsHelper
   def sign_in(user)
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
-    user.update_attribute(:remember_token, User.digest(remember_token))
+    user.not_validate_password = true
+    user.update_attributes(remember_token: User.digest(remember_token))
+    user.not_validate_password = false
     self.current_user = user
   end
 
@@ -20,8 +22,10 @@ module SessionsHelper
   end
 
   def sign_out
-    current_user.update_attribute(:remember_token,
+    current_user.not_validate_password = true
+    current_user.update_attributes(remember_token:
                                   User.digest(User.new_remember_token))
+    current_user.not_validate_password = false
     cookies.delete(:remember_token)
     self.current_user = nil
   end
