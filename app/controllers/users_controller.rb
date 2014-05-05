b112e5ec11b1af4e462d0ffcc78e1be84c67b9d0
nguyentuyen <nguyentuyen@nguyentuyen-Satellite-L740.(none)>
2014-05-05 16:59:20 +0700

    show_edit_destroy user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,41 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user,
+                only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate page: params[:page]
+  end
+
   def show
-    @user = User.find(params[:id])
+    @user = User.find params[:id]
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
   end
   
   def new
     @user = User.new
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   def create
-    @user = User.new(user_params)
+    @user = User.new user_params
     if @user.save
       flash[:success] = "Signup successfull!"
       redirect_to @user
@@ -22,5 +49,21 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find params[:id]
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
